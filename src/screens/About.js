58d8d7bfdaa1f3ebcd4f1fde39eb64b938ce258c
yvadher <yagniktechmail@gmail.com>
2018-03-07 01:07:54 -0800

    Add header bar for current page nagivgation

--- src/screens/About.js
+++ src/screens/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Container, Header, Content, Icon } from 'native-base';
 
-export default class Schedule extends React.Component {
+export default class About extends React.Component {
   render() {
     return (
       <View style={styles.container}>
