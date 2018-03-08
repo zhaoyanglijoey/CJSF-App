2c0c3a14c19b834b315f9878c1f8799ab234f4fc
ahad <ahadc@sfu.ca>
2018-03-07 23:40:54 -0800

    Code refactored, constants file created for about text and resources

---
+++ src/config/constants.js
@@ -0,0 +1,23 @@
+/* Constant resources used by the application */
+// Text and Constants for About Screen //
+export const LOGO = require('../../res/assets/ic_launcher-web.png');
+export const LOGO_HEIGHT = 260;
+export const LOGO_WIDTH = 260;
+
+export const TEXT_SIZE_HEADER=14;
+export const TEXT_SIZE_BODY=12;
+export const IOS_STATUS_BAR=24;
+
+export const ABOUT_HEADER1='Diverse * Independent * Yours'
+export const ABOUT_BODY1='CJSF 90.1 FM is Vancouver’s independent indie music, '
++ 'public affairs and social justice radio station. CJSF Radio located at '
++ 'Simon Fraser University’s Burnaby campus and broadcasts at 90.1FM or '
++ 'Greater Vancouver and via the internet to the world.  CJSF is operated by '
++ 'a small staff over 150 volunteers from the campus and the community.';
+export const ABOUT_HEADER2='Be the Media'
+export const ABOUT_BODY2_1='Want to get involved, attend one of our Orientations'
++ ' for New Volunteers. There are several every month check the website at ';
+export const CJSF_URL = 'http://www.cjsf.ca/';
+export const ABOUT_BODY2_2= 'for the next upcoming orientations.';
+export const ABOUT_BODY3='CJSF 90.1FM Radio is supported and made possible by '
++ 'the students of Simon Fraser University';
--- src/screens/About.js
+++ src/screens/About.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, Platform, Image} from 'react-native';
-import { Linking,} from 'react-native';
+import { StyleSheet, Text, View, Platform, Image, Linking} from 'react-native';
 import { Container, Header, Content, Icon } from 'native-base';
-
-const IMG_PATH = '../../res/assets/splashscreen.png'
-const CJSF_URL = 'http://www.cjsf.ca/'
+import { LOGO, LOGO_HEIGHT, LOGO_WIDTH, CJSF_URL, ABOUT_HEADER1,
+ABOUT_BODY1, ABOUT_HEADER2, ABOUT_BODY2_1, ABOUT_BODY2_2, ABOUT_BODY3,
+TEXT_SIZE_BODY, TEXT_SIZE_HEADER, IOS_STATUS_BAR}from '../config/constants.js';
 
 export default class Schedule extends React.Component {
   render() {
@@ -14,30 +13,16 @@ export default class Schedule extends React.Component {
         <Content>
             <Icon name='information-circle'/>
         </Content>
-        <Image source={require(IMG_PATH)}
-          style={{width: 275, height: 275,}}/>
-        <Text style={styles.header}>Diverse * Independent * Yours</Text>
-
-        <Text style={styles.body}>
-          CJSF 90.1 FM is Vancouver’s independent indie music, public affairs and social justice radio station.
-          CJSF Radio located at Simon Fraser University’s Burnaby campus and broadcasts at 90.1FM or Greater Vancouver and via the internet to the world.
-          CJSF is operated by a small staff over 150 volunteers from the campus and the community.
-        </Text>
-
-        <Text style={styles.header}>Be the Media</Text>
-
-        <Text style={styles.body}>
-          Want to get involved, attend one of our Orientations for New Volunteers.
-          There are several every month; check the website at
-
+        <Image source={LOGO} style={styles.logo}/>
+        <Text style={styles.header}>{ABOUT_HEADER1}</Text>
+        <Text style={styles.body}>{ABOUT_BODY1}</Text>
+        <Text style={styles.header}>{ABOUT_HEADER2}</Text>
+        <Text style={styles.body}>{ABOUT_BODY2_1}
           <Text style={{color: 'cornflowerblue'}}
             onPress={() => {Linking.openURL(CJSF_URL)}}> www.cjsf.ca </Text>
-
-          <Text> for the next upcoming orientations.</Text>
+          <Text>{ABOUT_BODY2_2}</Text>
         </Text>
-        <Text style={styles.body}>
-          CJSF 90.1FM Radio is supported and made possible by the students of Simon Fraser University
-        </Text>
+        <Text style={styles.body}>{ABOUT_BODY3}</Text>
       </View>
     );
   }
@@ -50,18 +35,22 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: Platform.OS === 'ios' ?  24 : 0,
+    paddingTop: Platform.OS === 'ios' ? IOS_STATUS_BAR : 0,
+  },
+  logo: {
+    width: LOGO_HEIGHT,
+    height: LOGO_WIDTH,
   },
   header: {
-    fontSize: 14,
+    fontSize: TEXT_SIZE_HEADER,
     fontWeight:'bold',
-    paddingBottom: 12
+    paddingBottom: TEXT_SIZE_BODY,
   },
   body: {
-    fontSize: 12,
-    paddingLeft: 14,
-    paddingRight: 14,
+    fontSize: TEXT_SIZE_BODY,
+    paddingLeft: TEXT_SIZE_HEADER,
+    paddingRight: TEXT_SIZE_HEADER,
     textAlign: 'justify',
-    paddingBottom: 12
+    paddingBottom: TEXT_SIZE_BODY
   },
 });
